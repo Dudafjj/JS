2faea1698d87c3b88acb757d4d78c05b7b8e0d0e
Dudafjj <102737580+Dudafjj@users.noreply.github.com>
2022-05-05 09:01:30 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+//Exercicios - Eduarda Ferreira Jorge
+//1-A
+console.log(10 + 20 * 30)
+
+//1-B
+
+console.log(4**2/30)
+
+//1-C
+
+console.log((9**4+2)*6-1)
+
+//2-A
+console.log(10 % 3* 10** 1 + 1 - 10 * 4 / 2)
+
+//3
+
+console.log('Eduarda')
+
+//4
+
+console.log(2*3*3*5)
+
+//5
+
+const n1 = 3;
+const n2 = 5;
+const n3 = 7;
+
+console.log(n1+n2+n3);
+
+//6
+
+const salario = 1800
+
+console.log(salario*2)
+
